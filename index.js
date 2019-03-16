02c2641e282d71c739930ae1ce517576eeba9083
brettmadrid <brettmadrid9@gmail.com>
2019-03-16 13:24:27 -0700

    Created bears table and added CRUD endpoints - Stretch complete

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+// ZOOS TABLE ENDPOINTS ***********************
 // GET ENPOINT
 server.get('/zoos', async (req, res) => {
   try {
@@ -68,6 +69,76 @@ server.put('/zoos/:id', async (req, res) => {
 server.delete('/zoos/:id', async (req, res) => {
   try {
     const deleted = await db('zoos')
+    .where( { id: req.params.id })
+    .del()
+
+    if (deleted > 0) {
+      res.status(204).json({ message: 'Successfully deleted!'});
+    } else {
+      res.status(404).json({ message: "Records not found"})
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Houston, we have a problem! Failed to delete record."});
+  }
+}) // END ZOOS TABLE ENDPOINTS
+
+// BEARS TABLE ENDPOINTS
+// GET ENPOINT
+server.get('/bears', async (req, res) => {
+  try {
+    const bears = await db('bears')
+    res.status(200).json(bears)
+  } catch (error) {
+    res.status(500).json({ message: "Houston, we have a problem! Failed to retrieve records."})
+  }
+})
+
+// GET by ID ENPOINT
+server.get('/bears/:id', async (req, res) => {
+  try {
+    const bears = await db('bears')
+    .where('id', req.params.id)
+    res.status(200).json(bears)
+  } catch (error) {
+    res.status(500).json({ message: "Houston, we have a problem! Failed to retrieve records."})
+  }
+})
+
+// POST ENDPOINT
+server.post('/bears', async (req, res) => {
+  try {
+    const [ id ] = await db('bears').insert(req.body)
+    if (id > 0) {
+      res.status(201).json({ message: 'record/s added' })
+    } else {
+      res.status(500).json({ message: "Houston, we have a problem! Failed to insert record."});
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Houston, we have a problem! Failed to insert record."});
+  }
+});
+
+// PUT ENDPOINT
+server.put('/bears/:id', async (req, res) => {
+  try {
+    const id = await db('bears')
+    .where({ id: req.params.id })
+    .update( req.body )
+
+    if (id > 0) {
+      res.status(201).json({ message: "Successfully updated!"});
+      } else {
+        res.status(404).json({ message: "Record not found to update"})
+      }
+  } catch (error) {
+    res.status(500).json({ message: "Houston, we have a problem! Failed to insert record."});
+  }
+})
+
+// DELETE ENDPOINT
+server.delete('/bears/:id', async (req, res) => {
+  try {
+    const deleted = await db('bears')
     .where( { id: req.params.id })
     .del()
 
